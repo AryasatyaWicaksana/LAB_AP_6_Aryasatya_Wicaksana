8885a97e416442cff1c1b339dae3af00dd3cb4fe
AryasatyaWicaksana <163378010+AryasatyaWicaksana@users.noreply.github.com>
2024-06-04 19:42:57 +0700

    file commit

--- pertemuan 4/do_while.cpp
+++ pertemuan 4/do_while.cpp
@@ -4,14 +4,21 @@ using namespace std;
 
 int main() {
     int angka = 1;
+
+    // Loop do-while pertama: mencetak angka 1 sampai 10
     do {
-        cout << angka << " ";
-        angka++;
-    } while(angka <= 10);
+        cout << angka << " ";  // Mencetak nilai angka saat ini
+        angka++;  // Increment angka
+    } while(angka <= 10);  // Kondisi loop: selama angka kurang dari atau sama dengan 10
+
+    cout << endl;  // Menambahkan baris baru untuk memisahkan output dari dua loop
 
     angka = 1;
+    // Loop do-while kedua: hanya mencetak angka 1 sekali karena kondisi awal sudah false
     do {
-        cout << angka << " ";
-        angka++;
-    } while(angka > 10);
+        cout << angka << " ";  // Mencetak nilai angka saat ini
+        angka++;  // Increment angka
+    } while(angka > 10);  // Kondisi loop: selama angka lebih besar dari 10 (kondisi awal false)
+
+    return 0;  // Mengembalikan nilai 0 untuk menunjukkan program selesai dengan sukses
 }
